0fcea2a29e8e71a4f7992c1a3c6291cfd91312d8
Rutilius <rutilius.imr@gmail.com>
2022-06-03 10:49:06 +0200

    Added logs before and after posting comments

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const Waline = require('@waline/vercel');
 
 const app = Waline({
   secureDomains: ['localhost', '127.0.0.1', 'blog.rutilius.dev'],
+  async preSave(comment) {
+    console.log("PreSave Log: " + comment);
+  },
   async postSave(comment) {
-    // do what ever you want after save comment
+    console.log("PostSave Log: " + comment);
   },
 });
 
