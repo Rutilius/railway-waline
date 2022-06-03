6c3024e8181aac8af5b86f1e45fb1f56c02e3b2b
Rutilius <rutilius.imr@gmail.com>
2022-06-03 10:47:52 +0200

    added secure domains

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Waline = require('@waline/vercel');
 
 const app = Waline({
+  secureDomains: ['localhost', '127.0.0.1', 'blog.rutilius.dev'],
   async postSave(comment) {
     // do what ever you want after save comment
   },
